ffcab8f88604767e19d7f2e734c4a0ab078fb361
Jose Ardila <44269547+Kenichi6221@users.noreply.github.com>
2021-08-20 14:44:17 -0500

    Revert "[pocket]: [create] Create pocket entity server side #1"

--- server/src/api_graphql/typeDefs.ts
+++
@@ -1,6 +0,0 @@
-import path from 'path'
-import { importSchema } from 'graphql-import'
-
-const typeDefs = importSchema(path.resolve(__dirname, './schema.graphql'))
-
-export default typeDefs
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,51 @@
-import { ApolloServer } from 'apollo-server'
-import { config } from 'dotenv'
-import logger from './configurations/logger'
-import resolvers from './api_graphql/mainResolver'
-import typeDefs from './api_graphql/typeDefs'
-import connectDB from './configurations/db'
+import { ApolloServer, gql } from 'apollo-server'
 
-config()
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+
+const typeDefs = gql`
+	# Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+	# This "Book" type defines the queryable fields for every book in our data source.
+	type Book {
+		title: String
+		author: String
+	}
+
+	# The "Query" type is special: it lists all of the available queries that
+	# clients can execute, along with the return type for each. In this
+	# case, the "books" query returns an array of zero or more Books (defined above).
+	type Query {
+		books: [Book]
+	}
+`
+
+const books = [
+	{
+		title: 'The Awakening',
+		author: 'Kate Chopin'
+	},
+	{
+		title: 'City of Glass',
+		author: 'Paul Auster'
+	}
+]
+
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+	Query: {
+		books: () => books
+	}
+}
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({ typeDefs, resolvers })
 
-connectDB()
-	.then(() => server.listen())
-	.then(({ url }: { url: string }) => {
-		logger.info(`🚀  Server ready at ${url}`)
-	})
-	.catch((error) => logger.error(`we have an error:${error}`))
+// The `listen` method launches a web server.
+server.listen().then(({ url }: { url: string }) => {
+	// eslint-disable-next-line no-console
+	console.log(`🚀  Server ready at ${url}`)
+})
